f1fdc10e3f53355f0b1839877019b17980f386b4
Artem Tartakynov <artem.tad@gmail.com>
2023-11-26 16:42:11 -0500

    update remove_hallucinations.py

--- remove_hallucinations.py
+++ remove_hallucinations.py
@@ -5,13 +5,16 @@
 
 import sys
 
+LOOKBACK_COUNT = 3
+
 def main():
     if len(sys.argv) != 2:
         print(f"Usage: python {sys.argv[0]} <srt_path>")
 
     in_path = sys.argv[1]
 
-    prev_text = None
+    prev_texts = []
+
     with open(in_path, 'r', encoding='utf-8') as file:
         current_position = 0
         while True:
@@ -25,13 +28,15 @@ def main():
                     else:
                         break
 
-                if text != prev_text:
+                if text not in prev_texts:
                     current_position += 1
                     print(current_position)
                     print(timestamp)
                     print(text)
 
-                    prev_text = text
+                prev_texts.append(text)
+                if len(prev_texts) > LOOKBACK_COUNT:
+                    prev_texts.pop(0)
             else:
                 break
 
