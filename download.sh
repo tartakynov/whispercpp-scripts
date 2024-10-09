3009c96d988007025e45090aeb4de1416533d7c1
Artem Tartakynov <artem.tad@gmail.com>
2024-10-08 21:15:52 -0400

    make the download script work for now

--- download.sh
+++ download.sh
@@ -18,5 +18,7 @@ cd whisper.cpp
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -U ane_transformers openai-whisper coremltools
+pip3 install "numpy<2"
+pip3 install torch==2.1.0
 
 ./models/generate-coreml-model.sh $1
